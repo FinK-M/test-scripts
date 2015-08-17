5045e7e45a4d639d80e1ad8f377e03c7ac78610c
Finlay Knops-Mckim <finlayknopsmckim@gmail.com>
2015-08-17 15:42:40 -0400

    Solved issue with left over bytes in read buffer
    Each command now reads status and message bytes rather than leaving them
    to cause errors in other read commands

--- mono_test.py
+++ mono_test.py
@@ -24,13 +24,20 @@ properties = ["Position:",
               "No. of Gratings:",
               "Current Units:",
               "Serial No."]
-q_bytes = list(range(7)) + [13, 14, 19]
+q_bytes = [0]  # list(range(7)) + [13, 14, 19]
 i = 0
+
+cm.write(chr(16).encode())
+high, low = divmod(5500, 0x100)
+sleep(0.1)
+cm.write(chr(high).encode())
+sleep(0.1)
+cm.write(chr(low).encode())
+sleep(0.1)
 for q in q_bytes:
 
-    cm.flush()
     cm.write(chr(56).encode())
-    sleep(0.01)
+    sleep(0.1)
     cm.write(chr(q).encode())
     raw = []
 
@@ -38,7 +45,7 @@ for q in q_bytes:
     for j in range(cm.inWaiting()):
         raw += cm.read()
     data = int(hex(raw[0]) + hex(raw[1]).replace('0x', ''), 16)
-    print(properties[i], data)
+    print(raw, properties[i], data)
     i += 1
 
 # print("Currently at {0} nm".format(data/10))
